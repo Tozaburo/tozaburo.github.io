af121eadd0bd901baa5792cb999d4308d657c60d
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-11-29 21:42:28 +0900

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -6,24 +6,46 @@ fetch(endpoint)
   .then(response => response.json())
   .then(data => {
     projects = data;
+    // 初期設定
+    updateCheckboxListener();
     clang();
   });
 
+var lang = window.navigator.language;
+
+function updateCheckboxListener() {
+  document.getElementById("slider-checkbox").addEventListener("change", checkboxChanged);
+}
+
+function checkboxChanged() {
+  if (lang == "ja") {
+    lang = "en";
+  } else {
+    lang = "ja";
+  }
+  clang();
+}
+
 function clang() {
   document.querySelector(".button-list").innerHTML = "";
+  var checkbox = document.getElementById('slider-checkbox');
   if (lang != "ja") {
-    document.title = "Projects";
-    document.querySelector("#title").innerHTML = "Projects";
     for (var n = 0; n < projects.length; n++) {
       if (projects[n][1] != "") {
+        document.querySelector("#title").innerHTML = "Projects";
         document.querySelector(".button-list").innerHTML += `<a href="${projects[n][2]}" class="button">${projects[n][1]}</a>`;
       }
     }
-  } else {
+    document.getElementById('slider-checkbox').checked = true;
+  } else if (lang == "ja") {
     for (var n = 0; n < projects.length; n++) {
       if (projects[n][0] != "") {
+        document.querySelector("#title").innerHTML = "作品";
         document.querySelector(".button-list").innerHTML += `<a href="${projects[n][2]}" class="button">${projects[n][0]}</a>`;
       }
     }
+    document.getElementById('slider-checkbox').checked = false;
   }
+
+  updateCheckboxListener(); // DOM変更後にリスナーを再設定
 }
