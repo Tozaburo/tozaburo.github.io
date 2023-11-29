cbc920b4790e7dd22b0d34655da00ac450b06ea6
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-11-29 21:32:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-var lang = window.navigator.language;
-
-window.addEventListener("DOMContentLoaded", function () {
-  clang();
-});
-
-function clang() {
-  if (lang != "ja") {
-    document.querySelector("#title").innerHTML = "Tozaburo | Toe Kobayashi";
-    document.querySelector(".main").innerHTML = `
-    <div class="text">
-      <div class="title">
-        <h1>Tozaburo</h1>
-        <h2>Toe Kobayashi</h2>
-      </div>
-      <div class="button-list">
+var englishMainHtml = `
+<div class="text">
+  <div class="title">
+    <h1>Tozaburo</h1>
+    <h2>Toe Kobayashi</h2>
+  </div>
+  <div class="button-list">
 <!--         <a href="https://tozaburo.github.io/who" class="button">Who Are You?</a> -->
-        <a href="https://tozaburo.github.io/activity" class="button">What Are You Doing?</a>
-        <a href="https://tozaburo.github.io/projects" class="button">What is Your Projects?</a>
-      </div>
-    </div>
-    `;
-  }
-}
+    <a href="https://tozaburo.github.io/activity" class="button">What Are You Doing?</a>
+    <a href="https://tozaburo.github.io/projects" class="button">What is Your Projects?</a>
+  </div>
+</div>
+`;
+
+var japaneseMainHtml = `<div class="text">
+<div class="title">
+  <h1>Tozaburo</h1>
+  <h2>小林都央</h2>
+</div>
+<div class="button-list">
+<!--         <a href="https://tozaburo.github.io/who" class="button">誰ですか？</a> -->
+  <a href="https://tozaburo.github.io/activity" class="button">何をやっていますか？</a>
+  <a href="https://tozaburo.github.io/projects" class="button" onhover="clang()">何を公開していますか？</a>
+</div>
+</div>`;
